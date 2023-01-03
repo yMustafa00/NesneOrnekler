7c0698a3aa1a71bb796dfe1b8bda485913d39da3
BIL-LAB1PC14\Ogrenci <123@123.COM>
2023-01-03 16:54:58 +0300

    no message

---
+++ U3_S_81/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_S_81
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ucgen ucgen = new Ucgen();
+            ucgen.A = 3;
+            ucgen.B = 4;
+            ucgen.B = 5;
+            Console.WriteLine("Ucgenin a kenar uzunluğu: {0}", ucgen.A);
+            Console.WriteLine("Ucgenin b kenar uzunluğu: {0}", ucgen.B);
+            Console.WriteLine("Ucgenin c kenar uzunluğu: {0}", ucgen.C);
+
+            Console.ReadLine();
+        }
+    }
+
+
+
+    public class Ucgen
+    {
+        int a;
+        int b;
+        int c;
+
+        public int A
+        {
+            get { return a; }
+            set
+            {
+                if (value <= 0)
+
+                    Console.WriteLine("Hatalı Bilgi");
+                else
+                    a = value;
+
+            }
+        }
+        public int B
+        {
+            get { return b; }
+            set
+            {
+                if (value <= 0)
+                    Console.WriteLine("Hatakı Bilgi");
+                else
+                    b = value;
+            }
+        }
+        public int C
+        {
+            get { return c; }
+            set
+            {
+                if (value <= 0)
+                    Console.WriteLine("Hatalı Bilgi");
+                else
+                    c = value;
+            }
+        }
+    }
+}
---
+++ U3_S_86/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_S_86
+{
+    class SayiBulucu
+    {
+        public int BuyukOlaniBul(int sayi1, int sayi2)
+        {
+            int sonuc;
+            if (sayi1 < sayi2)
+                sonuc = sayi2;
+            else
+                sonuc = sayi1;
+            return sonuc;
+        }
+
+
+    }
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+            SayiBulucu sb = new SayiBulucu();
+            int a = 100;
+            int b = 25;
+            int sonuc = sb.BuyukOlaniBul(a, b);
+            Console.WriteLine("Büyük Olan Sayı:{0}", sonuc);
+
+            Console.ReadLine();
+        }
+    }
+}
