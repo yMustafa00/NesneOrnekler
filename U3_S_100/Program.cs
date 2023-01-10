e21e6f76c216ecbb065360bd0ce660e2dd945d48
BIL-LAB5PC08\Ogrenci <123@123.com>
2023-01-10 12:20:43 +0300

    no message

---
+++ U3_S_100/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_S_100
+{
+    class OkulPersoneli
+    {
+        public string Ad { get; set; }
+
+        public string Soyad { get; set; }
+    }
+
+    class Ogretmen : OkulPersoneli
+    {
+        public string Brans { get; set; }
+    }
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            OkulPersoneli per = new OkulPersoneli();
+            Ogretmen ogrt = new Ogretmen();
+
+            ogrt.Ad = "Mustafa";
+            ogrt.Soyad = "Öztürk";
+            ogrt.Brans = "Bilişim";
+            Console.WriteLine("Öğretmen Sınıfında Türetilen {0} {1} {2}", ogrt.Ad, ogrt.Soyad, ogrt.Brans);
+
+
+            per.Ad = "Ramazan";
+            per.Soyad = "Güneş;";
+            Console.WriteLine("Okul Personeli Sınıfından Tüketilen {0} {1} {2}", per.Ad, per.Soyad);
+        }
+    }
+}
