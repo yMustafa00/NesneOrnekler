180ae8f65dededfff3f57ddfcb06db30a8f0b7da
BIL-LAB6PC06\Ogrenci <123132@gmail.com>
2023-02-21 13:25:43 +0300

    no message

---
+++ U4_S2/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U4_S2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] isimler = new string[5];
+        int index = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                isimler[index] = textBox1.Text;
+                index++;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Lütfen Beşten Fazla İsim Girmeyiniz");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                for (int i = 0; i < isimler.Length; i++)
+                {
+                    listBox1.Items.Add(isimler[i]);
+                }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
---
+++ U4_Uyg3/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U4_Uyg3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] isimler = new string[5];
+        int[] notlar = new int[5];
+        int index = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                isimler[index] = textBox1.Text;
+                notlar[index] = int.Parse(textBox2.Text);
+                index++;
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Bir Hata Oluştu Lütfen Tekrar Deneyiniz");
+                throw;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                for (int i = 0; i < isimler.Length; i++)
+                {
+                    if (isimler[i] != null)
+                    {
+                        listBox1.Items.Add(isimler[i] + ">" + notlar[i]);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Bir Hata Oluştu Lütfen Tekrar Deneyiniz");
+            }
+        }
+    }
+}
