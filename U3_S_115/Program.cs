76caf35c889c027a1aed7c8ad19393321c782881
BIL-LAB6PC06\Ogrenci <mustafa123@gmail.com>
2023-02-21 10:05:47 +0300

    no message

---
+++ U3_S_115/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_S_115
+{
+    class Program
+    {
+
+        enum Seviyeler
+        {
+            Çok_Düşük,
+            Düşük,
+            Orta,
+            Yüksek,
+            Çok_Yüksek,
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Seviyeler.Çok_Yüksek);
+            Console.WriteLine((int)Seviyeler.Çok_Yüksek);
+
+            Console.ReadLine();
+        }
+    }
+}
