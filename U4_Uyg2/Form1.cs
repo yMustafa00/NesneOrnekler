d38b1d87ec0fd359347927ba12990f826ef6f774
BIL-LAB1PC05\Ogrenci <123@123.COM>
2023-02-28 09:23:20 +0300

    no message

---
+++ U4_Uyg2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U4_Uyg2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] isimler = new string[5];
+        int index = 0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < isimler.Length; i++)
+            {
+                listBox1.Items.Add(isimler[i]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isimler[index] = textBox1.Text;
+            index++;
+        }
+    }
+}
