e46e962ee1c58d78537b2baa19a52cb84802e92d
BIL-LAB1PC05\Ogrenci <123@123.COM>
2023-02-28 16:18:13 +0300

    no message

---
+++ U4_Uyg13/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿
+namespace U4_Uyg13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumara1 = new System.Windows.Forms.TextBox();
+            this.btnSayiEkle = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnMetinEkle = new System.Windows.Forms.Button();
+            this.txtAdSoyad1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNumara2 = new System.Windows.Forms.TextBox();
+            this.txtAdSoyad2 = new System.Windows.Forms.TextBox();
+            this.btnNesneEkle = new System.Windows.Forms.Button();
+            this.lbSayi = new System.Windows.Forms.ListBox();
+            this.lbMetin = new System.Windows.Forms.ListBox();
+            this.lbNesne = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numara";
+            // 
+            // txtNumara1
+            // 
+            this.txtNumara1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.txtNumara1.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumara1.Location = new System.Drawing.Point(98, 15);
+            this.txtNumara1.Name = "txtNumara1";
+            this.txtNumara1.Size = new System.Drawing.Size(100, 22);
+            this.txtNumara1.TabIndex = 1;
+            // 
+            // btnSayiEkle
+            // 
+            this.btnSayiEkle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnSayiEkle.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSayiEkle.Location = new System.Drawing.Point(98, 53);
+            this.btnSayiEkle.Name = "btnSayiEkle";
+            this.btnSayiEkle.Size = new System.Drawing.Size(100, 23);
+            this.btnSayiEkle.TabIndex = 2;
+            this.btnSayiEkle.Text = "Sayı Ekle";
+            this.btnSayiEkle.UseVisualStyleBackColor = false;
+            this.btnSayiEkle.Click += new System.EventHandler(this.btnSayiEkle_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label2.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(236, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ad Soyad";
+            // 
+            // btnMetinEkle
+            // 
+            this.btnMetinEkle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnMetinEkle.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMetinEkle.Location = new System.Drawing.Point(312, 53);
+            this.btnMetinEkle.Name = "btnMetinEkle";
+            this.btnMetinEkle.Size = new System.Drawing.Size(100, 23);
+            this.btnMetinEkle.TabIndex = 4;
+            this.btnMetinEkle.Text = "Metin Ekle";
+            this.btnMetinEkle.UseVisualStyleBackColor = false;
+            // 
+            // txtAdSoyad1
+            // 
+            this.txtAdSoyad1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.txtAdSoyad1.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAdSoyad1.Location = new System.Drawing.Point(312, 15);
+            this.txtAdSoyad1.Name = "txtAdSoyad1";
+            this.txtAdSoyad1.Size = new System.Drawing.Size(100, 22);
+            this.txtAdSoyad1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label3.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(486, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Numara";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label4.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(486, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Ad Soyad";
+            // 
+            // txtNumara2
+            // 
+            this.txtNumara2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.txtNumara2.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumara2.Location = new System.Drawing.Point(572, 15);
+            this.txtNumara2.Name = "txtNumara2";
+            this.txtNumara2.Size = new System.Drawing.Size(100, 22);
+            this.txtNumara2.TabIndex = 8;
+            // 
+            // txtAdSoyad2
+            // 
+            this.txtAdSoyad2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.txtAdSoyad2.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAdSoyad2.Location = new System.Drawing.Point(572, 55);
+            this.txtAdSoyad2.Name = "txtAdSoyad2";
+            this.txtAdSoyad2.Size = new System.Drawing.Size(100, 22);
+            this.txtAdSoyad2.TabIndex = 9;
+            // 
+            // btnNesneEkle
+            // 
+            this.btnNesneEkle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnNesneEkle.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNesneEkle.Location = new System.Drawing.Point(572, 91);
+            this.btnNesneEkle.Name = "btnNesneEkle";
+            this.btnNesneEkle.Size = new System.Drawing.Size(100, 23);
+            this.btnNesneEkle.TabIndex = 10;
+            this.btnNesneEkle.Text = "Nesne Ekle";
+            this.btnNesneEkle.UseVisualStyleBackColor = false;
+            this.btnNesneEkle.Click += new System.EventHandler(this.btnNesneEkle_Click);
+            // 
+            // lbSayi
+            // 
+            this.lbSayi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lbSayi.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSayi.FormattingEnabled = true;
+            this.lbSayi.ItemHeight = 15;
+            this.lbSayi.Location = new System.Drawing.Point(27, 125);
+            this.lbSayi.Name = "lbSayi";
+            this.lbSayi.Size = new System.Drawing.Size(162, 274);
+            this.lbSayi.TabIndex = 11;
+            // 
+            // lbMetin
+            // 
+            this.lbMetin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lbMetin.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMetin.FormattingEnabled = true;
+            this.lbMetin.ItemHeight = 15;
+            this.lbMetin.Location = new System.Drawing.Point(227, 125);
+            this.lbMetin.Name = "lbMetin";
+            this.lbMetin.Size = new System.Drawing.Size(173, 274);
+            this.lbMetin.TabIndex = 12;
+            // 
+            // lbNesne
+            // 
+            this.lbNesne.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lbNesne.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNesne.FormattingEnabled = true;
+            this.lbNesne.ItemHeight = 15;
+            this.lbNesne.Location = new System.Drawing.Point(420, 125);
+            this.lbNesne.Name = "lbNesne";
+            this.lbNesne.Size = new System.Drawing.Size(172, 274);
+            this.lbNesne.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(1029, 466);
+            this.Controls.Add(this.lbNesne);
+            this.Controls.Add(this.lbMetin);
+            this.Controls.Add(this.lbSayi);
+            this.Controls.Add(this.btnNesneEkle);
+            this.Controls.Add(this.txtAdSoyad2);
+            this.Controls.Add(this.txtNumara2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAdSoyad1);
+            this.Controls.Add(this.btnMetinEkle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSayiEkle);
+            this.Controls.Add(this.txtNumara1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumara1;
+        private System.Windows.Forms.Button btnSayiEkle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnMetinEkle;
+        private System.Windows.Forms.TextBox txtAdSoyad1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNumara2;
+        private System.Windows.Forms.TextBox txtAdSoyad2;
+        private System.Windows.Forms.Button btnNesneEkle;
+        private System.Windows.Forms.ListBox lbSayi;
+        private System.Windows.Forms.ListBox lbMetin;
+        private System.Windows.Forms.ListBox lbNesne;
+    }
+}
+
---
+++ U4_Uyg13/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U4_Uyg13
+{
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<int> numaralarList = new List<int>();
+        List<string> adsoyadList = new List<string>();
+        List<Ogrenciler> ogrencilerList = new List<Ogrenciler>();
+
+        private void btnSayiEkle_Click(object sender, EventArgs e)
+        {
+            numaralarList.Add(int.Parse(txtNumara1.Text));
+            lbSayi.Items.Clear();
+            foreach (var item in numaralarList)
+            {
+                lbSayi.Items.Add(item);
+            }
+        }
+
+        private void btnNesneEkle_Click(object sender, EventArgs e)
+        {
+            Ogrenciler ogrenci = new Ogrenciler();
+            ogrenci.Numara = int.Parse(txtNumara2.Text);
+            ogrenci.AdSoyad = txtAdSoyad2.Text;
+            ogrencilerList.Add(ogrenci);
+            lbNesne.Items.Clear();
+            foreach (var item in ogrencilerList)
+            {
+                lbNesne.Items.Add(item.Numara + "" + item.AdSoyad);
+            }
+        }
+    }
+}
